d6b67a0091c7208ce0e9b6d3c119e6ad34fd3489
JYE <utael706@gmail.com>
2023-11-21 12:02:52 +0900

    Update modify2.py
    chat gpt edit

--- modify2.py
+++ modify2.py
@@ -14,27 +14,27 @@ def remove_text(target_text, text_to_remove):
 def main():
     st.title("Text Editor")
 
-    target_text = st.text_area("Enter your text:", "")
-    text_to_remove = st.text_input("Enter text to remove:", "")
+    target_text = st.text_area("텍스트를 입력하세요:", "")
+    text_to_remove = st.text_input("제거할 텍스트를 입력하세요:", "")
 
-    if st.button("Remove"):
+    if st.button("제거"):
         if target_text and text_to_remove:
             new_text = remove_text(target_text, text_to_remove)
-            st.subheader("Modified Text:")
-            st.text_area("Result:", value=new_text)  # 결과가 입력창에 나타남
+            st.subheader("수정된 텍스트:")
+            st.text_area("결과:", value=new_text)  # 결과가 입력창에 나타남
 
-            # Copy 'new_text' to the clipboard
-            st.experimental_set_query_params(new_text=new_text)  # Set new_text as query parameter
-            st.experimental_rerun()  # Rerun the app with the query parameter
+            # 클립보드로 'new_text' 복사
+            st.experimental_set_query_params(new_text=new_text)  # new_text를 쿼리 매개변수로 설정
+            st.experimental_rerun()  # 쿼리 매개변수를 사용하여 앱 재실행
 
-            st.success("Modified text copied to clipboard!")
+            st.success("수정된 텍스트가 클립보드에 복사되었습니다!")
         else:
-            st.warning("Please enter both the target text and text to remove.")
+            st.warning("대상 텍스트와 제거할 텍스트를 모두 입력하세요.")
 
-    # Retrieve the new_text query parameter and copy it to clipboard
+    # 'new_text' 쿼리 매개변수를 가져와서 클립보드로 복사
     new_text_query_param = st.experimental_get_query_params().get("new_text", None)
     if new_text_query_param:
-        st.experimental_rerun()  # Rerun the app without displaying any changes
+        st.experimental_rerun()  # 변경사항을 표시하지 않고 앱을 재실행
 
 if __name__ == "__main__":
     main()
